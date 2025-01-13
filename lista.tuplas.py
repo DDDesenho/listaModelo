8c35e7d350f99c5dbb352288850c02a4f2572798
Leo Menassa <leo.menassa2014@gmail.com>
2025-01-13 15:16:40 -0300

    lista de exemplo de array e tuplas , que no Python é lista

---
+++ lista.tuplas.py
@@ -0,0 +1,8 @@
+# Criando uma tupla
+coordenadas = (10, 20)
+
+# Acessando elementos
+print(f"X: {coordenadas[0]}, Y: {coordenadas[1]}")
+
+# Tuplas são úteis para constantes:
+constantes_fisicas = (3.14, 9.8, 6.626e-34)  # Pi, gravidade, constante de Planck
